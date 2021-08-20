5a10a133b2efadeb852281cfa8086b47e163de27
Alex Luciano Roesler Rese <alexrese@outlook.com>
2021-08-19 21:05:28 -0300

    exemplo forms c#

---
+++ aula_form_win/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace aula_form_win
+{
+    partial class From1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbParteUm = new System.Windows.Forms.TextBox();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.tbParteDois = new System.Windows.Forms.TextBox();
+            this.tbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite alguma coisa:";
+            // 
+            // tbParteUm
+            // 
+            this.tbParteUm.Location = new System.Drawing.Point(101, 78);
+            this.tbParteUm.Name = "tbParteUm";
+            this.tbParteUm.Size = new System.Drawing.Size(100, 20);
+            this.tbParteUm.TabIndex = 1;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(101, 105);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 2;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // tbParteDois
+            // 
+            this.tbParteDois.Location = new System.Drawing.Point(226, 78);
+            this.tbParteDois.Name = "tbParteDois";
+            this.tbParteDois.Size = new System.Drawing.Size(100, 20);
+            this.tbParteDois.TabIndex = 3;
+            // 
+            // tbResultado
+            // 
+            this.tbResultado.Location = new System.Drawing.Point(101, 148);
+            this.tbResultado.Name = "tbResultado";
+            this.tbResultado.Size = new System.Drawing.Size(310, 20);
+            this.tbResultado.TabIndex = 4;
+            // 
+            // From1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbResultado);
+            this.Controls.Add(this.tbParteDois);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.tbParteUm);
+            this.Controls.Add(this.label1);
+            this.Name = "From1";
+            this.Text = "Formulário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbParteUm;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.TextBox tbParteDois;
+        private System.Windows.Forms.TextBox tbResultado;
+    }
+}
+
---
+++ aula_form_win/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula_form_win
+{
+    public partial class From1 : Form
+    {
+        public From1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            if (tbParteUm.Text != "")
+            {
+                var valor1 = Convert.ToDouble(tbParteUm.Text);
+                var valor2 = Convert.ToDouble(tbParteDois.Text);
+                //double soma = valor1 + valor2;
+                tbResultado.Text = Convert.ToString(soma(valor1, valor2));
+             }
+            //MessageBox.Show("Digite um valor");
+        }
+
+        private double soma(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+    }
+}
